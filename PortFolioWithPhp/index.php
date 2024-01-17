f2da828bee17ad3822990b9cdc64b4b1adb4a5a0
Al-Mamun Salauddin <63588230+Mamun-71@users.noreply.github.com>
2024-01-17 19:46:04 +0600

    Add files via upload

---
+++ PortFolioWithPhp/index.php
@@ -0,0 +1,309 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- font awesome cdn -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+    <!-- css -->
+    <link rel="stylesheet" href="style.css">
+
+    <!-- bootstrap-5 cdn -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+
+
+    <title>Portfolio</title>
+    
+</head>
+
+
+<body>
+
+    <!-- navbar desgin start here -->
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+        <div class="container-fluid">
+
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <a class="navbar-brand" href="#">
+            <img src="images/logo.png" class="rounded-circle" width="100px">
+          </a>
+
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+
+              <li class="nav-item">
+                <a class="nav-link active mx-2" aria-current="page" href="#">Home</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="#about-me-id">About Me</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="#skills-id">Skills</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="#contact-me-id">Contact Me</a>
+              </li>
+
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+    <!-- navbar ends here -->
+
+     <!-- profile design starts here -->
+    <div class="profile" style="padding: 1% 3%;">
+
+        <div class="text-center">
+            <img src="images/weather.png" alt="top weather icon">
+        </div>
+
+        <div class="profile-desc justify-content-center">
+            <h3 class="text-center"> I am a Compettive Programmer <br> & <br> Trainee Software Engineer</h3>
+            <h5 class="text-center text-secondary">Hi! My name is Al-Mamun Salaudiinn. Welcome to my personal page that <br>
+                I’ve designed to showcase my skills and expertise that I’ve accumulated over the week. </h5>
+        </div>
+
+        <div class="text-center">
+            <img src="images/weather.png" alt="top weather icon">
+        </div>
+
+    </div>
+     <!-- profile design ends here -->
+
+     <!-- about me start here -->
+    <div class="container"  id="about-me-id">
+
+        <div class="mt-3">
+            <h3 class="text-center text-black p-2">ABOUT ME</h3>
+        </div>
+  
+        <hr class="dotted-hr"> 
+
+
+        <div class="row">
+
+            <div class="aboutme-image col p-6 text-center">
+                <img src="images/almamun-photo.JPG" class="rounded-circle border" width="200px" height="220px">
+            </div>
+    
+            <div class="aboutme-desc col p-6">
+                <p class="fw-bold"> I like to call myself “a passionate problem solver”. </p>
+                <p>I solved lot of problems on different online judge.Now I started my carrer with SoftBd.I am very excited that i started my professional carrer.</p> 
+                
+                <hr class="aboutme-hr">
+                <p>Name: Al-Mamun Salauddin</p> 
+                <hr class="aboutme-hr">
+                <p>Address: Dhaka, Bangladesh</p>
+                <hr class="aboutme-hr">
+                <p class="text-dark">Email: almamunsalauddin@gmail.com </p>
+                <hr class="aboutme-hr">
+                <p>Phone: +8801609353793 </p>
+                <hr class="aboutme-hr">
+                <hr class="aboutme-hr">
+               
+                <p>
+                    <a href="https://drive.google.com/file/d/12nQg7aiJLPtvPgZTzk6D44gF5zTP0BxL/view?usp=sharing" 
+                    target="blank" class="btn btn-danger btn-md" role="button">Download CV</a>
+                </p>
+    
+            </div>
+        </div>
+    
+
+    </div>
+    <!-- about me ends here -->
+
+    <!-- professional experience start here  -->
+
+
+    <div class="container mt-5 mb-5">
+
+        <div class="row">
+            
+            <!--Work Experience CARD HERE-->
+
+            <div class="col-lg-4 ">
+
+                <div class="card">
+                    <div class="card-header text-center">
+                        <h2>Work Experience</h2>
+                    </div>
+                    <div class="card-body">
+                        <ol>
+                            <li>
+                                <strong>Trainee Software Engineer </strong><br>
+                                02/01/2024–Present <br>
+                                SoftBd Ltd. <br>
+                                Bangla Motor, Dhaka (Bangladesh) <br> 
+                            </li>
+                            <li>
+                                <strong>Programming Trainer of CSE Department</strong> <br>
+                                10/11/2020–02/02/2023<br>
+                                Sylhet Engineering College <br>
+                                Tilagarh, Alurtol Road,Sylhet (Bangladesh) <br>
+                            </li>
+                        </ol>
+                    </div>
+                  </div>
+            </div>
+
+            <!--Education CARD HERE-->
+            <div class="col-lg-4">
+              
+                <div class="card">
+                    <div class="card-header text-center">
+                        <h2>Education</h2>
+                    </div>
+                    <div class="card-body">
+                        <ol>
+                        
+                            <li>
+                               <strong>B.Sc. in Computer Science & Engineering</strong> <br>
+                               Sylhet Engineering College 2018-2023</br>
+                                Tilagarh, Alurtol Road,Sylhet (Bangladesh)<br>
+                                <a href="https://www.sec.ac.bd/" class="btn btn-primary" target="_blank">Website</a><br>
+                                
+                               Result: A (3.50/4.00) <br>
+                            </li>
+                            <li>
+                               <strong>HSC</strong> <br>
+                               2013-2016 <br>
+                               Govt. Science College <br>
+                               Tejgaon,Dhaka (Bangladesh)<br>
+                            </li>
+                            <li>
+                               <strong>SSC</strong> <br>
+                               2007-2013 <br>
+                               Madertek Abdul Aziz School & College <br>
+                               Sabujbag,Dhaka (Bangladesh)<br>
+                            </li>
+                       </ol>
+                      
+                    </div>
+                  </div>
+    
+            </div>
+
+            <!-- Skills CARD HERE -->
+
+            <div class="col-lg-4">
+
+                <div class="card" id="skills-id">
+                    <div class="card-header text-center">
+                        <h2>Work Experience</h2>
+                    </div>
+                    <div class="card-body">
+                        <ul>
+                            <li><p><strong>C/C++</strong></p></li>
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-striped progress-bar-animated" style="width:60%;"> </div>
+                            </div>
+
+                            <li><p><strong>Java</strong></p></li>
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-striped progress-bar-animated" style="width:40%;"> </div>
+                            </div>
+
+                            <li><p><strong>Python</strong></p></li>
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-striped progress-bar-animated" style="width:35%;"> </div>
+                            </div>
+
+                            <li><p><strong>Data Structure</strong></p></li>
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-striped progress-bar-animated" style="width:80%;"> </div>
+                            </div>
+
+                            <li><p><strong>Algorithms</strong></p></li>
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-striped progress-bar-animated" style="width:75%;"> </div>
+                            </div>
+
+                            <li><p><strong>MySql</strong></p></li>
+                            <div class="progress">
+                                <div class="progress-bar progress-bar-striped progress-bar-animated" style="width:40%;"> </div>
+                            </div>
+                        </ul>
+                    </div>
+                  </div>
+            </div>
+
+        </div>
+    </div>
+
+    <!-- professional experience ends here  -->
+
+     <!-- achievement,contact me start here -->
+
+    <div class="row text-white mx-3 my-3 " style="background-color:gray;">
+
+        <div class="achievement col-lg-4 px-3 py-3">
+            <h2>ACHIEVEMENTS</h2>
+            <hr class="dotted-hr">
+            <p>1. SEC Intra Campus Programming Contest CSE Fest 20202, <br> Rank : 2nd  Runners Up</p>
+            <p>2. ICPC Dhaka Regional 2021 Online Preliminary Contest, <br> Rank : 77 out of 1324 teams (Compete among top 6%)</p>
+            <p>3. ICPC Dhaka Regional 2022 Online Preliminary Contest, <br> Rank : 96 out of 1747 teams(Compete among top 5%).</p>
+            <p>4. Purple Coder (4 Star) at CodeChef</p>
+
+        </div>
+        <div class="contact col-lg-4 px-3 py-3 " id="contact-me-id">
+            <h2>CONTACT ME</h2>
+            <hr class="dotted-hr">
+
+            <form action="mailto:almamunsalauddin@gmail.com" method="post" enctype="text/plain">
+                <p><input type="text" placeholder="Your Name" name="name"></p>
+                <p><input type="email" placeholder="Your Email" name="email"></p>
+                <textarea name="message" id="" cols="30" rows="10" placeholder="Your Message Here"></textarea>
+                <button class="btn btn-success" type="submit">SEND</button>
+                <button class="btn btn-success" type="reset">RESET</button>
+            </form>
+        </div>
+
+        <div class="contact-links col-lg-4">
+            <h2>MORE WAYS TO CONTACT</h2>
+            <hr class="dotted-hr">
+
+            <div class="media-buttons">
+                <button class="btn" onclick="window.open('https://www.linkedin.com/in/al-mamun-salauddin/')" class="btn request-callback"><i class="fa fa-linkedin" style="color:  #3b5998;" aria-hidden="true"></i></button>
+                <button class="btn" onclick="window.open('https://github.com/Mamun-71')" class="btn request-callback"><i class="fa fa-github" aria-hidden="true" style="color: #000000;"></i></button>
+            </div>
+        </div>
+    </div>
+
+    <!-- achievement-contact ends here  -->
+
+     <!-- footer starts from here  -->
+
+     <div class="container">
+
+     <div class="footer row">
+        <div class="footer-menu col-6">
+            <a href="#">Home</a>
+            <a href="#about-me-id">About Me</a>
+            <a href="#skill-id">Skill</a>
+            <a href="#contact-me-id">Contact Me</a>
+        </div>
+
+        <div class="footer-desc col-6">
+            &copy; Mamun 2023. Developed with <span style="color: red; font-size: 1.5rem;">❤</span> by Mamun.
+        </div>
+    </div>
+
+</div>
+    <!-- footer ends here  -->
+     
+    
+</body>
+</html>
